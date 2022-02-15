e60b849a9d82be116b6e25bb21f6ae06cd699165
Rayanne Barros <rayanneb126@gmail.com>
2022-02-15 16:33:04 -0300

    Exercícios

---
+++ script.js
@@ -0,0 +1,62 @@
+// Utilizando a API https://viacep.com.br/ws/${CEP}/json/
+// crie um formulário onde o usuário pode digitar o cep
+// e o endereço completo é retornado ao clicar em buscar
+
+const inputCep = document.querySelector('#cep');
+const btnCep = document.querySelector('#btnCep');
+const resultadoCep = document.querySelector('.resultadoCep');
+
+btnCep.addEventListener('click', handleClick)
+
+function handleClick(e) {
+  e.preventDefault();
+  const cep = inputCep.value;
+  buscaCep(cep)
+}
+
+function buscaCep(cep) {
+  fetch(`https://viacep.com.br/ws/${cep}/json/`)
+    .then(res => res.text())
+    .then(body => {
+      resultadoCep.innerText = body;
+    })
+};
+
+
+// Utilizando a API https://blockchain.info/ticker
+// retorne no DOM o valor de compra da bitcoin and reais.
+// atualize este valor a cada 30s
+
+const btcDisplay = document.querySelector('.btc');
+
+function fetchBtc() {
+  fetch('https://blockchain.info/ticker')
+    .then(res => res.json())
+    .then(btcJson => {
+      btcDisplay.innerText = ('R$ ' + btcJson.BRL.buy).replace('.', ',')
+    })
+}
+
+// setInterval(fetchBtc, 1000 * 30);
+
+fetchBtc();
+
+
+
+// Utilizando a API https://api.chucknorris.io/jokes/random
+// retorne uma piada randomica do chucknorris, toda vez que
+// clicar em próxima
+
+const btnProxima = document.querySelector('.proxima');
+const paragrafoPiada = document.querySelector('.piada');
+
+function puxarPiada() {
+  fetch('https://api.chucknorris.io/jokes/random')
+    .then(res => res.json())
+    .then(piada => {
+      paragrafoPiada.innerHTML = piada.value;
+    })
+}
+
+btnProxima.addEventListener('click', puxarPiada);
+
